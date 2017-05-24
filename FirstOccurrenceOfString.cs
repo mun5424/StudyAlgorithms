99cc02d2dc4d8f478e252fd2872555bf2f1a5438
Charles Oh <Charles.Oh@bristolfarms.com>
2017-05-24 10:43:14 -0700

    Multiple Occurrence of integers

---
+++ FirstOccurrenceOfString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndChill
+{
+    class FirstOccurrenceOfString
+    {
+        //find the first occurrence of string y in string x.
+        //classic interview question - use Knuth-Morris-Pratt Algorithm 
+    
+    }
+}
