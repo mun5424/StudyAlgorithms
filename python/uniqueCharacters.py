97b8ff446876c7118bd791a7e83ff4b3457124ff
Charlie Oh <mun5424@gmail.com>
2019-09-01 00:26:34 -0700

    find all unique characters in a string

--- python/uniqueCharacters.py
+++ python/uniqueCharacters.py
@@ -1,12 +1,19 @@
 # determine if a string has all unique characters
 
 def uniqueCharacters(word):
-    chars = [256]
+    chars = [False] * 128
+    for i in word: 
+        letterIndex = ord(i)
+        if chars[letterIndex]:
+            return False
+        else:
+            chars[letterIndex] = True
+    return True
 
 
     
     
         
 
-print(compressString('asdsadkjasxd')) # fail
-print(compressString('abcde')) # pass
+print(uniqueCharacters('asdsadkjasxd')) # fail
+print(uniqueCharacters('abcde')) # pass
