d97cfec954e5b52bf3afee7675c39fa6744a4192
Charlie Oh <charlie@Charlies-iMac.local>
2019-08-31 23:57:48 -0700

    unique characters

---
+++ python/uniqueCharacters.py
@@ -0,0 +1,12 @@
+# determine if a string has all unique characters
+
+def uniqueCharacters(word):
+    chars = [256]
+
+
+    
+    
+        
+
+print(compressString('asdsadkjasxd')) # fail
+print(compressString('abcde')) # pass
